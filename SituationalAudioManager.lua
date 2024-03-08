90d6e94a8fbbb639bcd50bd1b9c7d1603b40a3a5
Alec <Alec.Gamble@live.co.uk>
2024-03-08 15:54:33 +0000

    Fixed a typo in intro description

--- SituationalAudioManager.lua
+++ SituationalAudioManager.lua
@@ -1013,7 +1013,7 @@ local generalOptions = {
         welcomeMessage = {
             name = "Welcome to the Situational Audio Manager (SAM for short)!\n\n"
             .."This addon is designed to allow seamless transition between various audio balancing profiles depending on what type of content you are enjoying. For example you might want to hear the game music during a raid but listen to your own music when doing world content or have the game muted except for cutscenes.\n\n"
-            .."By default your audio configuration will have been automatically imported into the Default tab which the addon will use to set your volume. |cffff8000Whilst using the addon you must set your volume preferneces via the addon and not the blizzard options interface as those settings will be overriden!!|r\n\n"
+            .."By default your audio configuration will have been automatically imported into the Default tab which the addon will use to set your volume. |cffff8000Whilst using the addon you must set your volume preferences via the addon and not the blizzard options interface as those settings will be overriden!!|r\n\n"
             .."If you would like to override the volume settings for a partiular activity or cutscenes, select overrides to add below and then adjust their settings on the relevant tabs.\n\n"
             .."Multiple profiles can be set up to allow you to switch between various presets.\n\n"
             .."If you have any requests for further features or more overrides please let me know and I'll do my best to implement them!\n\n"
